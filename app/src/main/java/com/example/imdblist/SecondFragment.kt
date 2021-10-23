8da6d00ee30739ce159981929b18393fda7aba33
Rubin Shrestha <rubinshrestha14@gmail.com>
2021-10-23 11:58:56 +0545

    Implemented second fragment with sortByDescending

--- app/src/main/java/com/example/imdblist/SecondFragment.kt
+++ app/src/main/java/com/example/imdblist/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +18,120 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SecondFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SecondFragment : Fragment() {   private lateinit var imdbAdapter: ImdbAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var imdbDataList: MutableList<ImdbData>
+
+    private var myView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        if (myView == null) {
+            myView = container?.inflate(R.layout.fragment_first)
+        }
+        return myView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        imdbDataList = ArrayList()
+        imdbDataList.add(
+            ImdbData(
+                "Dune (2021)",
+                8.8,
+                8,
+                88,
+                false,
+                "https://cdn.europosters.eu/image/1300/posters/dune-fear-is-the-mind-killer-i107183.jpg"
+
+            )
+        )
+        imdbDataList.add(
+            ImdbData(
+                "Venom (2021)",
+                7.2,
+                7,
+                71,
+                false,
+                "https://m.media-amazon.com/images/I/71-GNRNf0VS._AC_SL1000_.jpg"
+            )
+        )
+        imdbDataList.add(
+            ImdbData(
+                "Free Guy (2021)",
+                9.0,
+                9,
+                92,
+                false,
+                "https://img.republicworld.com/republic-prod/stories/images/16019654225f7c0d6e3ec0a.png"
+            )
+        )
+        imdbDataList.add(
+            ImdbData(
+                "The Suicide Squad (2021)",
+                8.5,
+                8,
+                85,
+                false,
+                "https://cdn.shopify.com/s/files/1/0037/8008/3782/products/TheSuicideSquadOneSheetMoviePoster-145571_1024x1024@2x.jpg?v=1622506700"
+
+            )
+        )
+        imdbDataList.add(
+            ImdbData(
+                "Black Widow (2021)",
+                8.1,
+                8,
+                81,
+                false,
+                "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
+            )
+        )
+        imdbDataList.add(
+            ImdbData(
+                "The Conjuring 3 (2021)",
+                8.0,
+                8,
+                80,
+                false,
+                "https://m.media-amazon.com/images/M/MV5BYzQxMjVkZjEtMDQxYy00MGIwLWE1NjYtYzZkZmU1NTFlZGFlXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg"
+            )
+        )
+
+        imdbAdapter = ImdbAdapter(FirstFragment(), imdbDataList) { position ->
+            val imdbData = imdbDataList[position]
+            if (imdbData.bookmarked){
+                imdbData.bookmarked = false
+            }else{
+                imdbData.bookmarked = true
+            }
+            imdbAdapter.notifyItemChanged(position)
+        }
+
+        imdbDataList.sortByDescending { it.ratingMetascoreTV }
+        recyclerView.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = imdbAdapter
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            FirstFragment().apply {
+
             }
     }
+
 }
